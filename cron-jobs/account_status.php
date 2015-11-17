35571bcae1d42c5f0f1c11ca7b4be06b26bcd0f1
mlpari001 <matthew@lymm.com>
2015-11-17 21:34:19 +0000

    removed facebook status

--- cron-jobs/account_status.php
+++ cron-jobs/account_status.php
@@ -1,5 +1,5 @@
 <?php
-//https://www.facebook.com/alzyaat
+
 function get_title($url){
     $str = file_get_contents($url);
     if(strlen($str)>0){
@@ -22,9 +22,6 @@ function get_tag( $attr, $value, $xml )
     return $matches[1];
 }
 
-$sql='';
-$query='';
-
 $db = mysql_connect("127.0.0.1", "root", "");
 $selected = mysql_select_db("opiceisis",$db);
 $result = mysql_query("SELECT id, twitter FROM isismembers WHERE twitterstatus=1");
@@ -40,26 +37,3 @@ while ($row = mysql_fetch_array($result))
         mysql_query("UPDATE isismembers SET twitterstatus = 0 WHERE id = $id");
     }
 }
-
-$result_facebook = mysql_query("SELECT id, facebook FROM isismembers WHERE facebookstatus=1");
-
-while ($row = mysql_fetch_array($result_facebook))
-{
-    $html = file_get_contents($row{'facebook'});
-    sleep(10);
-    echo $html;
-
-    //$extract = get_tag( 'class', 'accessible_elem', $html);
-    //echo $extract;
-}
-
-
-
-
-
-
-
-
-
-
-
