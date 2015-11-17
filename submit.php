4f3fa62883cfc6040bec0b8a701eeb4df50243a9
n1ghtl1ve <n1ghtl1ve@n1ghtys-MacBook-Pro.local>
2015-11-17 17:17:55 +0100

    Captcha
    Added Captcha to our Form

--- submit.php
+++ submit.php
@@ -1,10 +1,21 @@
 <?php
-//add captcha
+php session_start();
+include_once $_SERVER['DOCUMENT_ROOT'] . '/securimage/securimage.php';
+$securimage = new Securimage();
+if ($securimage->check($_POST['captcha_code']) == false) {
+  // the code was incorrect
+  // you should handle the error so that the form processor doesn't continue
+
+  // or you can use the following code if there is no validation or you do not know how
+  echo "The security code entered was incorrect.<br /><br />";
+  echo "Please go <a href='javascript:history.go(-1)'>back</a> and try again.";
+  exit;
+}
 $server = "localhost";
 $user = "USER";
 $pass = "PASS";
 $db = "opiceisis";
-$timestamp = date("Y-m-d H:i:s"); 
+$timestamp = date("Y-m-d H:i:s");
 
 $conn = mysql_connect($server, $user, $pass);
 if (!$conn)
@@ -28,6 +39,8 @@ if (count($_POST) < 1){
 	die("Not enough information on submit!");
 }
 */
+
+
 
 $query = "
 INSERT INTO `$db`.`isismembers` (`id`, `realname`, `location`, `twitter`, `facebook`, `youtube`, `other`, `dateadded`) VALUES (NULL, '$realname', '$loc', '$twitter', '$facebook', '$youtube', '$other', '$timestamp');";
