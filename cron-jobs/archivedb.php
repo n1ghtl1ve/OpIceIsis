674a2f5ef995031714779ffe6f2adea3688e0e99
root <root@aptgetswag.(none)>
2015-11-18 00:16:31 -0500

    archive in json dump

--- cron-jobs/archivedb.php
+++ cron-jobs/archivedb.php
@@ -11,18 +11,14 @@ if (!$conn)
         die('Could not connect: ' . mysql_error());
 }
 mysql_select_db($db, $conn);
-
-$query = "
-                SELECT realname, location, twitter, twitterstatus, facebook, youtube, other, dateadded FROM isismembers;";
-$result = mysql_query($query, $conn);
+$query = "SELECT * FROM isismembers;";
+$result = mysql_unbuffered_query($query);
 
 $file = fopen("../dump.txt", "w") or die ("Unable to open file!");
 
-fwrite($file, "Archived at: $timestamp\n");
-fwrite($file, "realname,location,twitter,twitterstatus,facebook,youtube,other,dateadded\n");
-
-while ($row = mysql_fetch_array($result, MYSQL_ASSOC)){
-                fwrite($file, "{$row['realname']},{$row['location']},{$row['twitter']},{$row['twitterstatus']},{$row['youtube']},{$row['other']},{$row['dateadded']}\n");
+while ($row = mysql_fetch_object($result)){
+	fwrite($file, json_encode($row));
+	fwrite($file, ",");
 }
 fclose($file);
 
